7c0a2c0e4650623fe0dc3ce313a458381ad16e9c
Manognya Koduganti <manognyakoduganti@Manognyas-MacBook-Pro.local>
2018-02-05 17:01:53 -0500

    added book-list container

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'JS: For beginners'},
+        {title: 'jodi picoult House Rules'},
+        {title: 'jodi picoult the pact'},
+        {title: 'jodi picoult my sisters keeper'},
+    ]
+}
